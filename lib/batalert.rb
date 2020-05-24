cc7394edadaf9cf990dd9ef2181659365faa53b2
Antonio Terceiro <asa@terceiro.xyz>
2020-05-24 12:12:43 -0300

    Update batalert.rb

--- lib/batalert.rb
+++ lib/batalert.rb
@@ -1,27 +1,38 @@
-require_relative "batalert/version"
+# $ ruby -I lib bin/mything
+
+
+require "batalert/version"
 require "espeak"
 require "libnotify"
 
 module Batalert
-  class Batalert
-    capacity = File.open('/sys/class/power_supply/BAT0/capacity', &:readline)
-    capacity = capacity.to_i
+  class Runner
+    def run
+      capacity = File.open('/sys/class/power_supply/BAT0/capacity', &:readline)
+      capacity = capacity.to_i
 
-    status = File.open('/sys/class/power_supply/BAT0/status', &:readline)
-    status = status.chomp
+      status = File.open('/sys/class/power_supply/BAT0/status', &:readline)
+      status = status.chomp
 
-    if capacity < 10 && status == 'Discharging'
-      notify = Libnotify.new(summary: "PUT ON CHARGING, YOUR BATTERY IS AT #{capacity}%.", timeout: 3, urgency: :critical)
-      notify.update
-      speech = ESpeak::Speech.new("put on charging, your battery is at #{capacity}%.")
-      speech.speak
-    end
+      if capacity < 10 && status == 'Discharging'
+        notify = Libnotify.new(summary: "PUT ON CHARGING, YOUR BATTERY IS AT #{capacity}%.", timeout: 3, urgency: :critical)
+        notify.update
+        speech = ESpeak::Speech.new("put on charging, your battery is at #{capacity}%.")
+        speech.speak
+      end
 
-    if capacity > 90 && status == 'Charging'
-      notify = Libnotify.new(summary: "REMOVE CHARGING, YOUR BATTERY IS AT #{capacity}%.", timeout: 3, urgency: :critical)
-      notify.update
-      speech = ESpeak::Speech.new("remove charging, your battery is at #{capacity}%.")
-      speech.speak
+      if capacity > 90 && status == 'Charging'
+        notify = Libnotify.new(summary: "REMOVE CHARGING, YOUR BATTERY IS AT #{capacity}%.", timeout: 3, urgency: :critical)
+        notify.update
+        speech = ESpeak::Speech.new("remove charging, your battery is at #{capacity}%.")
+        speech.speak
+      end
     end
   end
 end
+
+######## vv exe/batalert vv
+#!/usr/bin/env ruby
+require 'batalert'
+Batalert::Runner.new.run
+######## ^^ exe/batalert ^^
